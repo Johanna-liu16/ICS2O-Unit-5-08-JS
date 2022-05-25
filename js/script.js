2346786204468642f21fa040d3a6feb6158c8066
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-25 17:03:36 +0000

    pwa

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit-5-08-JS/sw.js", {
+    scope: "/ICS2O-Unit-5-08-JS/",
   })
 }
 
